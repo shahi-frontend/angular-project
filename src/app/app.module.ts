2153b73121dfff2ecd8fbfce8a5cc032b91ac0fe
Rupali Shahi <rupali.ceinc@gmail.com>
2017-12-24 01:32:06 +0530

    employee component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ const appRoutes: Routes = [
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule, HttpModule, RouterModule.forRoot(appRoutes) ],
-  declarations: [ AppComponent, HeaderComponent, FooterComponent, HomeComponent, SignupComponent, 
+  declarations: [ AppComponent, HeaderComponent, FooterComponent, HomeComponent, SignupComponent,
                   LoginComponent, PageNotFoundComponent, EmployeeComponent ],
   bootstrap:    [ AppComponent ]
 })
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -8,11 +8,29 @@ import { Component } from '@angular/core';
 
 export class EmployeeComponent{
     colSpan: number = 5;
-    employees: any[] = [
-        {code: 'emp101', name: 'Jessica', gender: 'female', annualSalary: 80000, dateOfBirth: '25/6/1998'},
-        {code: 'emp102', name: 'Harvey', gender: 'male', annualSalary: 76000, dateOfBirth: '01/04/2000'},
-        {code: 'emp103', name: 'Louis', gender: 'male', annualSalary: 60000, dateOfBirth: '25/08/2000'},
-        {code: 'emp104', name: 'Donna', gender: 'female', annualSalary: 58000, dateOfBirth: '20/4/2002'},
-        {code: 'emp105', name: 'Rachel', gender: 'female', annualSalary: 50000, dateOfBirth: '28/9/2005'},
-    ];
+    employees: any[];
+    constructor(){
+        this.employees = [
+            {code: 'emp101', name: 'Jessica', gender: 'female', annualSalary: 80000, dateOfBirth: '25/6/1998'},
+            {code: 'emp102', name: 'Harvey', gender: 'male', annualSalary: 76000, dateOfBirth: '01/04/2000'},
+            {code: 'emp103', name: 'Louis', gender: 'male', annualSalary: 60000, dateOfBirth: '25/08/2000'},
+            {code: 'emp104', name: 'Donna', gender: 'female', annualSalary: 58000, dateOfBirth: '20/4/2002'},
+            {code: 'emp105', name: 'Rachel', gender: 'female', annualSalary: 50000, dateOfBirth: '28/9/2005'},
+        ];
+    }
+
+    getEmployees(): void{
+        this.employees = [
+            {code: 'emp101', name: 'Jessica', gender: 'female', annualSalary: 80000, dateOfBirth: '25/6/1998'},
+            {code: 'emp102', name: 'Harvey', gender: 'male', annualSalary: 76000, dateOfBirth: '01/04/2000'},
+            {code: 'emp103', name: 'Louis', gender: 'male', annualSalary: 60000, dateOfBirth: '25/08/2000'},
+            {code: 'emp104', name: 'Donna', gender: 'female', annualSalary: 58000, dateOfBirth: '20/4/2002'},
+            {code: 'emp105', name: 'Rachel', gender: 'female', annualSalary: 50000, dateOfBirth: '28/9/2005'},
+            {code: 'emp106', name: 'Michael', gender: 'male', annualSalary: 55000, dateOfBirth: '14/5/2003'}
+        ];
+    }
+
+    trackByCode(index: number, employee:any): string{
+        return employee.code;
+    }
 }
